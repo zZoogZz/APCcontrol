d171dd96bfc03dbafff8658372594a41e9ba9e40
zZoogZz <19891289+zZoogZz@users.noreply.github.com>
2021-07-05 15:05:57 +1200

    Improved String Manipulation for the handler/

--- handler.py
+++ handler.py
@@ -4,20 +4,33 @@ def ch(string):
 	commands = ["on","blink"]
 	allcommands = ["off","sleep"]
 	#
+
 	if string == "\n": return("pass")
 	if string[0:] == "#": return("pass")
-	if string == "['all.sleep\n']": return ("time.sleep(1)")
+	string = string.strip()
+
 	try:
-		string = string[:-1]
 		string = string.split("@")
-		fullcommand = string[0].split(".")
-		colour = fullcommand[0]
-		command = fullcommand[1]
+	except: 
+		string[0] = string
+
+	fullcommand = string[0].split(".")
+	colour = fullcommand[0]
+	command = fullcommand[1]
+
+	try:
 		addresses = string[1]
 	except:
-		pass
-
-
+		addresses = 0
+	'''
+	print("Current Instruction Printout")
+	print(string)
+	print(fullcommand)
+	print(colour)
+	print(command)
+	print(addresses)
+	print("End of printout")
+	'''
 
 	if colour in colours: #Test that colours are allowed
 		coloursafe = True
@@ -25,14 +38,14 @@ def ch(string):
 			commandsafe = True
 		else:
 			commandsafe = False
-			print("Error with \"{}\". For the instruction \"{}\"".format(command,string))
+			print("Error with colour instruction type for \"{}\". For the instruction \"{}\"".format(command,string))
 	elif colour == "all": #Test that if the colour isn't allowed it is an all command
 		coloursafe = True
 		if command in allcommands: #Test that the allcommand command is allowed.
 			commandsafe = True
 		else:
 			commandsafe = False
-			print("Error with \"{}\". For the instruction \"{}\"".format(command,string))	
+			print("Error with all instruction type for \"{}\". For the instruction \"{}\"".format(command,string))	
 	else:
 		coloursafe = False
 		print("Error with \"{}\". For the instruction \"{}\"".format(colour,string))
@@ -41,8 +54,10 @@ def ch(string):
 		#Command Builder
 		if command == "sleep":
 			exc = "time.sleep(1)"
+			print("WAITING...")
 		else:
 			exc = "functions.{colr}.{func}([{addresses}],outport)".format(colr=colour, func=command, addresses=addresses)
+		print("EXC: {}".format(exc))
 		return(exc)
 	elif coloursafe == False or commandsafe == False:
 		print("\"{}\" - Command Skipped".format(string))
